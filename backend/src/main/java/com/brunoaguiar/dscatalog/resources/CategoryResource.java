d902418b5868291d115a54080844052d05819b98
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-01-18 22:00:51 +0000

    DTO

--- backend/src/main/java/com/brunoaguiar/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.brunoaguiar.dscatalog.entities.Category;
+import com.brunoaguiar.dscatalog.dto.CategoryDTO;
 import com.brunoaguiar.dscatalog.services.CategoryService;
 
 //use rest controller to make this class a resource 
@@ -21,8 +21,8 @@ public class CategoryResource {
 	private CategoryService service;
 	@GetMapping
 	//create a list with categories in it!
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/brunoaguiar/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.brunoaguiar.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.brunoaguiar.dscatalog.dto.CategoryDTO;
 import com.brunoaguiar.dscatalog.entities.Category;
 import com.brunoaguiar.dscatalog.repositories.CategoryRepository;
 
@@ -15,8 +17,12 @@ public class CategoryService {
 	//connect this service class to the categoryRepository class to fetch information from repository class 
 	@Autowired
 	private CategoryRepository repository;
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		//Lambda expression that transform list category into categoryDTO
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	
 		
 	}
 }
