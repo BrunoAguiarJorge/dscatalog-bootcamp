afa1bdcc2cea090bc23cb270875deb3fac25232b
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-01-19 17:41:41 +0000

    update category

--- backend/src/main/java/com/brunoaguiar/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.brunoaguiar.dscatalog.dto.CategoryDTO;
 import com.brunoaguiar.dscatalog.entities.Category;
 import com.brunoaguiar.dscatalog.repositories.CategoryRepository;
-import com.brunoaguiar.dscatalog.services.exceptions.EntityNotFoundException;
+import com.brunoaguiar.dscatalog.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -27,13 +29,11 @@ public class CategoryService {
 		//Lambda expression that transform list category into categoryDTO
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
-	
-		
 	}
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
 		return new CategoryDTO(entity);
 		
 	}
@@ -43,5 +43,18 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+	
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+		Category entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 }
