0e0306f3da7823adbfaf416170884b38a7d02dbe
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-01-20 19:06:00 +0000

    Pagination

--- backend/src/main/java/com/brunoaguiar/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/resources/CategoryResource.java
@@ -1,9 +1,11 @@
 package com.brunoaguiar.dscatalog.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,10 +29,19 @@ public class CategoryResource {
 	//let resource layer access service layer.
 	@Autowired
 	private CategoryService service;
+	
 	@GetMapping
-	//create a list with all categories in it!
-	public ResponseEntity<List<CategoryDTO>> findAll(){
-		List<CategoryDTO> list = service.findAll();
+	//create a list with all categories in it! ReuestParam -> used for pagination!
+	public ResponseEntity<Page<CategoryDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			){
+	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+		
 		return ResponseEntity.ok().body(list);
 	}	
 	@GetMapping(value = "/{id}")
