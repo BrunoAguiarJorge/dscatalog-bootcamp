7802ee19ea3086e51a184c5a549007b55ebcb688
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-01-23 19:24:06 +0000

    Add front-web

--- backend/src/main/java/com/brunoaguiar/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/resources/CategoryResource.java
@@ -25,58 +25,53 @@ import com.brunoaguiar.dscatalog.services.CategoryService;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-	
-	//let resource layer access service layer.
+
+	// let resource layer access service layer.
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping
-	//create a list with all categories in it! ReuestParam -> used for pagination!
+	// create a list with all categories in it! ReuestParam -> used for pagination!
 	public ResponseEntity<Page<CategoryDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			){
-	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
 		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
-		
+
 		return ResponseEntity.ok().body(list);
-	}	
+	}
+
 	@GetMapping(value = "/{id}")
-	//create a list with all categories in it!
-	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
 		CategoryDTO dto = service.findById(id);
-		//"ok" return http code 200
+		// "ok" return http code 200
 		return ResponseEntity.ok().body(dto);
-	}	
-	
+	}
+
 	@PostMapping
-	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
 		dto = service.insert(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-		.buildAndExpand(dto.getId()).toUri();
-		//created return http code 201
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		// created return http code 201
 		return ResponseEntity.created(uri).body(dto);
 
 	}
-	
+
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> insert(@PathVariable Long id, @RequestBody CategoryDTO dto){
+	public ResponseEntity<CategoryDTO> insert(@PathVariable Long id, @RequestBody CategoryDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 
-
 	}
+
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id){
+	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 
-
 	}
-	
-	
-	
+
 }
---
+++ front-web/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+    return (
+    <div>Bruno Aguiar </div>
+
+    );
+}
+
+export default App;
