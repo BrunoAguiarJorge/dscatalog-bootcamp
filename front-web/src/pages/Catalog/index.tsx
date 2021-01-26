72d8ccb57963c898dfc531121b2b04b9a634ff72
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-01-26 19:23:47 +0000

    Finished product listing layout

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import ProductCard from './components/productcard';
+import './styles.scss'
 
 const Catalog = () => (
-    <div> 
-        <h1>Catalog</h1>
+    <div className="catalag-container"> 
+        <h1 className="catalag-title">
+            Catalogo de products
+            </h1>
+            <div className="catalog-products">
+                <ProductCard />
+                  <ProductCard />
+                    <ProductCard />
+                <ProductCard />
+                  <ProductCard />
+                    <ProductCard />
+                <ProductCard />
+                  <ProductCard />
+                    <ProductCard />
+                <ProductCard />
+                  <ProductCard />
+                    <ProductCard />
+
+            </div>
     </div>
 );
 
