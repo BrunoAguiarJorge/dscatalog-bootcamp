fcaecf5f639872fe6b8680b27504c5beadc4fc44
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-02-10 18:08:02 +0000

    CRUD USER

---
+++ backend/src/main/java/com/brunoaguiar/dscatalog/resources/UserResource.java
@@ -0,0 +1,83 @@
+package com.brunoaguiar.dscatalog.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.brunoaguiar.dscatalog.dto.UserDTO;
+import com.brunoaguiar.dscatalog.dto.UserInsertDTO;
+import com.brunoaguiar.dscatalog.services.UserService;
+
+//use rest controller to make this class a resource 
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	//let resource layer access service layer.
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	//create a list with all categories in it! ReuestParam -> used for pagination!
+	public ResponseEntity<Page<UserDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "firstName") String orderBy
+			){
+	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<UserDTO> list = service.findAllPaged(pageRequest);
+		
+		return ResponseEntity.ok().body(list);
+	}	
+	@GetMapping(value = "/{id}")
+	//create a list with all categories in it!
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+		UserDTO dto = service.findById(id);
+		//"ok" return http code 200
+		return ResponseEntity.ok().body(dto);
+	}	
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto){
+		UserDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+		.buildAndExpand(newDto.getId()).toUri();
+		//created return http code 201
+		return ResponseEntity.created(uri).body(newDto);
+
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> insert(@PathVariable Long id, @RequestBody UserDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+
+
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
+
+	}
+	
+	
+	
+}
