853f76ab570231d9da28a21c62563574fb812628
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-02-10 20:49:12 +0000

    basic beans validation

--- backend/src/main/java/com/brunoaguiar/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/dto/UserDTO.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.brunoaguiar.dscatalog.entities.User;
 
 public class UserDTO implements Serializable{
@@ -13,8 +16,10 @@ public class UserDTO implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	private Long id;
+	@NotBlank(message = "Campo obrigatorio")
 	 private String firstName;
 	 private String lastName;
+	 @Email(message = "Favor entrar um email valido")
 	 private String email;
 	
 	 Set<RoleDTO> roles	= new HashSet<>()
