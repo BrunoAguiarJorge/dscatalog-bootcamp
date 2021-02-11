909d59a664315b0a5db15b95cc59d1f682b6083b
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-02-11 17:21:59 +0000

    Custom Constraint Validation

--- backend/src/main/java/com/brunoaguiar/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/dto/UserInsertDTO.java
@@ -1,7 +1,11 @@
 package com.brunoaguiar.dscatalog.dto;
 
+import com.brunoaguiar.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
+	
 	private static final long serialVersionUID = 1L;
 
 	private String password;
