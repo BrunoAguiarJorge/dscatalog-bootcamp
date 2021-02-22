db5efa0e683862cf7958666ecfe05c385b309888
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-02-22 14:26:18 +0000

    Search by product postgre compability fixed

--- backend/src/main/java/com/brunoaguiar/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.brunoaguiar.dscatalog.services;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -33,9 +35,9 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(Long categoryId, PageRequest pageRequest) {
-		Category category = (categoryId == 0) ? null : categoryRepository.getOne(categoryId);
-		Page<Product> list = repository.find(category, pageRequest);
+	public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest) {
+		List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
+		Page<Product> list = repository.find(categories, name.trim(), pageRequest);
 		//Lambda expression that transform list category into categoryDTO
 		return list.map(x -> new ProductDTO(x));
 		
