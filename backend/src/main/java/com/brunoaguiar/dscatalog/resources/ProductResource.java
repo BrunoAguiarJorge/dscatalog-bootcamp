904587894bbe7f6120bb14cbbb83a0796a55f0ae
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-02-23 14:50:47 +0000

    File upload prod

--- backend/src/main/java/com/brunoaguiar/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/resources/ProductResource.java
@@ -18,22 +18,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brunoaguiar.dscatalog.dto.ProductDTO;
+import com.brunoaguiar.dscatalog.dto.UriDTO;
 import com.brunoaguiar.dscatalog.services.ProductService;
 
 //use rest controller to make this class a resource 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
-	
-	//let resource layer access service layer.
+
+	// let resource layer access service layer.
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
-	//create a list with all categories in it! ReuestParam -> used for pagination!
+	// create a list with all categories in it! ReuestParam -> used for pagination!
 	public ResponseEntity<Page<ProductDTO>> findAll(
 			@RequestParam(value = "categoryId", defaultValue = "0") Long categoryId,
 			@RequestParam(value = "name", defaultValue = "") String name,
@@ -41,47 +43,49 @@ public class ProductResource {
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			){
-	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
 		Page<ProductDTO> list = service.findAllPaged(categoryId, name, pageRequest);
-		
+
 		return ResponseEntity.ok().body(list);
-	}	
+	}
+
 	@GetMapping(value = "/{id}")
-	//create a list with all categories in it!
-	public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
+	// create a list with all categories in it!
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
-		//"ok" return http code 200
+		// "ok" return http code 200
 		return ResponseEntity.ok().body(dto);
-	}	
-	
+	}
+
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
+	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-		.buildAndExpand(dto.getId()).toUri();
-		//created return http code 201
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		// created return http code 201
 		return ResponseEntity.created(uri).body(dto);
 
 	}
-	
+
+	@PostMapping(value = "/image")
+	public ResponseEntity<UriDTO> uploadImage(@RequestParam("file") MultipartFile file) {
+		UriDTO dto = service.uploadFile(file);
+		return ResponseEntity.ok().body(dto);
+	}
+
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto){
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 
-
 	}
+
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> delete(@PathVariable Long id){
+	public ResponseEntity<ProductDTO> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 
-
 	}
-	
-	
-	
+
 }
--- backend/src/main/java/com/brunoaguiar/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -11,56 +11,93 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.brunoaguiar.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> 
-	entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
-	HttpStatus status = HttpStatus.NOT_FOUND;
-	StandardError err = new StandardError();
-	err.setTimestamp(Instant.now());
-	err.setStatus(status.value());
-	err.setError("Resouce Not Found");
-	err.setMessage(e.getMessage());
-	err.setPath(request.getRequestURI());
-	return ResponseEntity.status(status).body(err);
-	
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resouce Not Found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+
 	}
-	
+
 	@ExceptionHandler(DataBaseException.class)
-	public ResponseEntity<StandardError> 
-	database(ResourceNotFoundException e, HttpServletRequest request){
-	HttpStatus status = HttpStatus.BAD_REQUEST;
-	StandardError err = new StandardError();
-	err.setTimestamp(Instant.now());
-	err.setStatus(status.value());
-	err.setError("Database Exception");
-	err.setMessage(e.getMessage());
-	err.setPath(request.getRequestURI());
-	return ResponseEntity.status(status).body(err);
-	
+	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+
 	}
-	
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ValidationError> 
-	validation(MethodArgumentNotValidException e, HttpServletRequest request){
-	HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-	ValidationError err = new ValidationError();
-	err.setTimestamp(Instant.now());
-	err.setStatus(status.value());
-	err.setError("Validation Exception");
-	err.setMessage(e.getMessage());
-	err.setPath(request.getRequestURI());
-	
-	for (FieldError f : e.getBindingResult().getFieldErrors()) {
-		err.addError(f.getField(), f.getDefaultMessage());
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		return ResponseEntity.status(status).body(err);
+
 	}
-	return ResponseEntity.status(status).body(err);
-	
+
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("AWS Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+
 	}
-	
-	
+
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("AWS Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<StandardError> illegalArgument(IllegalArgumentException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Bad Request");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+
+	}
+
 }
