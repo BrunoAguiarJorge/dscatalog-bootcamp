e14b2f27f39e95bf85f671eed739728d7fd581dc
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-05-29 12:26:15 +0100

    Fixing pagination issue when searching

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -3,22 +3,24 @@ import { Link } from 'react-router-dom';
 import ProductCard from './components/productcard';
 import './styles.scss'
 import { makeRequest } from '../../core/utils/request';
-import { ProductsResponse } from 'core/types/Products';
+import { Category, ProductsResponse } from 'core/types/Products';
 import ProductCardLoader from 'core/components/Loaders/ProductCardLoader';
-import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
+import ProductFilters from 'core/components/ProductFilters';
 import Pagination from 'core/components/Pagination';
 
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
     const [activePage, setActivePage] = useState(0);
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState<Category>();
 
-    const getProducts = useCallback((filter?: FilterForm) => {
+    const getProducts = useCallback(() => {
         const params = {
             page: activePage,
             linesPerPage: 12,
-            name: filter?.name,
-            categoryId: filter?.categoryId
+            name: name,
+            categoryId: category?.id
         }
         setIsLoading(true)
         makeRequest({ url: '/products', params })
@@ -26,10 +28,27 @@ const Catalog = () => {
             .finally(() => {
                 setIsLoading(false)
             })
-    }, [activePage]);
+    }, [activePage, name, category]);
+
     useEffect(() => {
       getProducts();
     }, [getProducts]);
+
+    const handleChangeName = (name: string) => {
+        setActivePage(0);
+        setName(name);
+    }
+
+    const handleChangeCategory = (category: Category) => {
+        setActivePage(0);
+        setCategory(category);
+    }
+
+    const clearFilters =() => {
+        setActivePage(0);
+        setCategory(undefined);
+        setName('');
+    }
 
     return (
         <div className="catalag-container">
@@ -37,7 +56,13 @@ const Catalog = () => {
                 <h1 className="catalag-title">
                     Product's Catalogue
             </h1>
-                <ProductFilters onSearch={filter => getProducts(filter)}/>
+                <ProductFilters 
+                name={name}
+                category={category}
+                handleChangeName={handleChangeName}
+                handleChangeCategory={handleChangeCategory}
+                clearFilters={clearFilters}
+                />
             </div>
 
             <div className="catalog-products">
