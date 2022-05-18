60c7f832abcafc8adda47dee598f5ff13232539c
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2022-05-18 21:08:40 +0100

    bean validation constraint implementation revision

--- backend/src/main/java/com/brunoaguiar/dscatalog/services/validation/UserInsertValidator.java
+++ backend/src/main/java/com/brunoaguiar/dscatalog/services/validation/UserInsertValidator.java
@@ -27,12 +27,10 @@ public class UserInsertValidator implements ConstraintValidator<UserInsertValid,
 
 		List<FieldMessage> list = new ArrayList<>();
 
-		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à
-		// lista
+		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
 		User user = repository.findByEmail(dto.getEmail());
 		if (user != null) {
 			list.add(new FieldMessage("email", "Email ja existe"));
-
 		}
 
 		for (FieldMessage e : list) {
